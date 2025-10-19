e8192f7e4f07a7ea956f0ba16aafb9eb4316b002
jm1ra <98293294+jm1ra@users.noreply.github.com>
2025-10-19 17:09:59 +1100

    Create Login Fragment Draft

---
+++ app/src/main/java/com/example/s8129361assignment2/LoginFragment.kt
@@ -0,0 +1,32 @@
+package com.example.s8129361assignment2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class LoginFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val loginButton = view.findViewById<Button>(R.id.buttonLogin)
+
+        loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
+        }
+    }
+}
